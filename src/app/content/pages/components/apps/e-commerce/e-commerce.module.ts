556603a574865e579c3af5c2de904660da6772aa
Muhammad Fawzy Habib <muhammad.habib.dev@gmail.com>
2019-02-23 17:20:23 +0200

    sync filtered data

--- src/app/content/pages/components/apps/e-commerce/e-commerce.module.ts
+++ src/app/content/pages/components/apps/e-commerce/e-commerce.module.ts
@@ -200,8 +200,8 @@ const routes: Routes = [
 		SpecificationEditDialogComponent,
 		ScrollableDirective,
 		MapComponent,
-		// LoginComponent,
-		// AuthNoticeComponent
+		LoginComponent,
+		AuthNoticeComponent
 	]
 })
 export class ECommerceModule { }
